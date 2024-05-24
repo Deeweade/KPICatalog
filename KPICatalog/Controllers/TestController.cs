c48a24c0db95aec037012db530fa103251a70cd5
Petrov2, Oleg <opetrov2@rencredit.ru>
2024-05-25 00:44:01 +0300

    Initial commit

---
+++ KPICatalog/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KPICatalog;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : Controller
+{
+    [HttpGet]
+    public string Index()
+    {
+        return "Hello world!";
+    }
+}
