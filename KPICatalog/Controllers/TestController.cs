4bff5c503afa7c89db6cdd055e02bfef9d2c4b53
Petrov2, Oleg <opetrov2@rencredit.ru>
2024-05-27 16:53:22 +0300

    added authorize attribute

--- KPICatalog/Controllers/TestController.cs
+++ KPICatalog/Controllers/TestController.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KPICatalog;
 
+[Authorize]
 [Route("api/[controller]")]
 [ApiController]
 public class TestController : Controller
