673a6fd098127b395c2f75d8c316779a5f618b6c
Petrov2, Oleg <opetrov2@rencredit.ru>
2024-06-21 13:40:41 +0300

    added migrations

--- KPICatalog.API/Migrations/20240621103955_NewEntities.Designer.cs
+++ KPICatalog.API/Migrations/20240621103955_NewEntities.Designer.cs
@@ -4,6 +4,7 @@ using KPICatalog.Infrastructure.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KPICatalog.API.Migrations
 {
     [DbContext(typeof(KPICatalogDbContext))]
-    partial class KPICatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240621103955_NewEntities")]
+    partial class NewEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
